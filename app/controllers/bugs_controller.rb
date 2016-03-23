e7a9d2fd27c8980031cb81d4fe0819856c7f05fd
AJ2ART <amarnath2art@gmail.com>
2016-03-23 22:34:57 +0000

    first

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -3,11 +3,18 @@ class BugsController < ApplicationController
   before_action :set_priorities
   before_action :set_statuses
   before_action :set_bug, only: [:show, :edit, :update, :destroy]
+  before_action :all_bugs, only:[:index, :create, :destroy, :update]
+  
+  
+  respond_to :html, :js
+  
+  def all_bugs
+    @bugs = Bug.all
+  end
 
   # GET /bugs
   # GET /bugs.json
   def index
-    @bugs = Bug.all
   end
 
   # GET /bugs/1
@@ -26,45 +33,64 @@ class BugsController < ApplicationController
 
   # POST /bugs
   # POST /bugs.json
+  
   def create
     @bug = Bug.new(bug_params)
-
-    respond_to do |format|
-      if @bug.save
-        format.html { redirect_to @bug, notice: 'Bug was successfully created.' }
-        format.json { render :show, status: :created, location: @bug }
-        author = Author.find(bug_params[:author_id])
-        @bug.build_author(:id => author.id)
-      else
-        format.html { render :new }
-        format.json { render json: @bug.errors, status: :unprocessable_entity }
-      end
-    end
+    author = Author.find(bug_params[:author_id])
+    @bug.build_author(:id => author.id)
+    @bug.save!
+    @current_bug = @bug
   end
+  
+  # def create
+  #   @bug = Bug.new(bug_params)
+
+  #   respond_to do |format|
+  #     if @bug.save
+  #       format.html { redirect_to @bug, notice: 'Bug was successfully created.' }
+  #       format.json { render :show, status: :created, location: @bug }
+  #       author = Author.find(bug_params[:author_id])
+  #       @bug.build_author(:id => author.id)
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @bug.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /bugs/1
   # PATCH/PUT /bugs/1.json
+  
   def update
-    respond_to do |format|
-      if @bug.update(bug_params)
-        format.html { redirect_to @bug, notice: 'Bug was successfully updated.' }
-        format.json { render :show, status: :ok, location: @bug }
-      else
-        format.html { render :edit }
-        format.json { render json: @bug.errors, status: :unprocessable_entity }
-      end
-    end
+   @bug.update!(bug_params)
   end
+
+  # def update
+  #   respond_to do |format|
+  #     if @bug.update(bug_params)
+  #       format.html { redirect_to @bug, notice: 'Bug was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @bug }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @bug.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /bugs/1
   # DELETE /bugs/1.json
+  
   def destroy
-    @bug.destroy
-    respond_to do |format|
-      format.html { redirect_to bugs_url, notice: 'Bug was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+   @bug.destroy!
   end
+
+  # def destroy
+  #   @bug.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to bugs_url, notice: 'Bug was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
